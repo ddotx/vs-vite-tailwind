82d49ea4cb63d0cc636b2cebb271e846a53fd2c4
dome_dexpc <domemacintosh@gmail.com>
2024-01-25 02:27:01 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import AppCard from '@/components/AppCard.vue';
+import AppInput from '@/components/AppInput.vue';
+import AppButton from '@/components/AppButton.vue';
+import TheMainNav from '@/components/TheMainNav.vue';
+import { useDark } from '@vueuse/core'
+import Flexbox from '@/components/Flexbox.vue';
+import Grid from '@/components/Grid.vue';
+
+const isDark = useDark()
+</script>
+
+<template>
+  <div class="m-10">
+    <input type="checkbox" v-model="isDark" />Dark Mode
+    <AppCard />
+    <TheMainNav />
+    <form action="">
+      <label> </label>
+      <AppInput label="Username" />
+      <AppButton>Submit</AppButton>
+    </form>
+    <p class="first-letter:text-5xl selection:bg-yellow-400">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti sequi
+      aspernatur deleniti, sed minus vero iusto repudiandae quaerat molestias
+      asperiores delectus, quis debitis explicabo voluptatibus. Adipisci quasi
+      vero error voluptates?
+    </p>
+    <ul class="list-disc marker:text-green-500 marker:text-2xl">
+      <li>Vue.js</li>
+      <li>React</li>
+      <li>Svelte</li>
+      <li>Solid</li>
+    </ul>
+    <Flexbox />
+    <Grid />
+  </div>
+</template>
---
+++ src/components/Grid.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="grid grid-cols-4 gap-2 grid-flow-dense">
+        <div></div>
+        <div></div>
+        <div class="col-span-3"></div>
+        <div class="flex items-center justify-around col-span-3">
+            <div class="w-16 h-16 !min-h-0 !bg-green-300"></div>
+            <div class="w-16 h-16 !min-h-0 !bg-blue-300"></div>
+            <div class="w-16 h-16 !min-h-0 !bg-green-300"></div>
+            <div class="w-16 h-16 !min-h-0 !invisiblebg-blue-300"></div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </section>
+</template>
+<style scoped>
+div {
+    @apply bg-gray-400 min-h-[100px] even:bg-gray-600;
+}
+</style>
